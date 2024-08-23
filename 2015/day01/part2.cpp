71aba2ccc0a339fc7e11a23a9c30a8a466bb0c19
gabrielzschmitz <gabrielzschmitz@protonmail.com>
2024-08-22 21:30:59 -0300

    ⭐ Fix debug flag at testing

--- 2015/day01/part2.cpp
+++ 2015/day01/part2.cpp
@@ -32,9 +32,18 @@
 
 bool debug = false;
 
+void checkDebugFlag(const std::string& input, bool& debug) {
+  size_t pos = input.find_last_of('/');
+
+  std::string last_part =
+    (pos == std::string::npos) ? input : input.substr(pos + 1);
+
+  if (last_part == "test") debug = true;
+}
+
 int main(int argc, char* argv[]) {
   std::string user_input = (argc == 2) ? argv[1] : "input";
-  if (user_input == "test") debug = true;
+  checkDebugFlag(user_input, debug);
 
   std::ifstream input_file(user_input);
   if (!input_file.is_open()) {
